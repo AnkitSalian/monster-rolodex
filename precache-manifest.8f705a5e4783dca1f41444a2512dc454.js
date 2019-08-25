2bc8877792978662dcef90e9fb4bf9c8ae7329bb
Ankit <salian.ankit@tcs.com>
2019-08-25 17:10:14 +0530

    Updates

---
+++ precache-manifest.8f705a5e4783dca1f41444a2512dc454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801ddd407b10455a3b84dffc663ce799",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6529c3c0347eba0df166",
+    "url": "/monster-rolodex/static/css/main.688b8e2b.chunk.css"
+  },
+  {
+    "revision": "f802219fca84ad4640ab",
+    "url": "/monster-rolodex/static/js/2.ea1710b8.chunk.js"
+  },
+  {
+    "revision": "6529c3c0347eba0df166",
+    "url": "/monster-rolodex/static/js/main.f22d28c1.chunk.js"
+  },
+  {
+    "revision": "02868fe8e38b073cb3a1",
+    "url": "/monster-rolodex/static/js/runtime~main.ec684995.js"
+  }
+]);
